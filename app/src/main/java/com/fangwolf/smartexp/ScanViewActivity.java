1d8f3ba4f86e025bd3b32dbeea61a8998fa82de6
FangWolf <siyu2510@163.com>
2018-03-03 23:40:14 +0800

    带手电筒的扫码界面

--- app/src/main/java/com/fangwolf/smartexp/ScanViewActivity.java
+++ app/src/main/java/com/fangwolf/smartexp/ScanViewActivity.java
@@ -4,14 +4,19 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
-public class ScanViewActivity extends AppCompatActivity {
+public class ScanViewActivity extends AppCompatActivity implements DecoratedBarcodeView.TorchListener{
 
     private CaptureManager capture;
     private DecoratedBarcodeView barcodeScannerView;
+    Button swichLight;
+    private boolean isLightOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,21 @@ public class ScanViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan_view);
 
         barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.dbv_custom);
+        barcodeScannerView.setTorchListener(this);
+        //手电筒控制
+        swichLight = findViewById(R.id.light);
+        swichLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isLightOn) {
+                    barcodeScannerView.setTorchOff();
+                } else {
+                    barcodeScannerView.setTorchOn();
+                }
+            }
+        });
 
-        capture = new CaptureManager(this, barcodeScannerView);
+        capture = new CaptureManager(this,barcodeScannerView);
         capture.initializeFromIntent(getIntent(), savedInstanceState);
         capture.decode();
     }
@@ -58,5 +76,19 @@ public class ScanViewActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    //手电开
+    @Override
+    public void onTorchOn() {
+        Toast.makeText(this, "开灯", Toast.LENGTH_SHORT).show();
+        isLightOn = true;
+    }
+
+    //手电关
+    @Override
+    public void onTorchOff() {
+        Toast.makeText(this, "关灯", Toast.LENGTH_SHORT).show();
+        isLightOn = false;
     }
 }
