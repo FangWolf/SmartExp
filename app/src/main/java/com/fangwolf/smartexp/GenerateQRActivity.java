ee40ef10446ec50351bdd2687eae49dd71a1a759
FangWolf <siyu2510@163.com>
2018-03-11 16:54:16 +0800

    扫描界面美化 省市区Spinner联动

--- app/src/main/java/com/fangwolf/smartexp/GenerateQRActivity.java
+++ app/src/main/java/com/fangwolf/smartexp/GenerateQRActivity.java
@@ -1,6 +1,9 @@
 package com.fangwolf.smartexp;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,10 +11,13 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
@@ -33,8 +39,14 @@ public class GenerateQRActivity extends AppCompatActivity {
     EditText Rphone;
     EditText RDadress;
 
-    Spinner Sadress;
-    Spinner Radress;
+    Spinner Sadress_province;
+    Spinner Sadress_city;
+    Spinner Sadress_district;
+    int a = 0;
+    Spinner Radress_province;
+    Spinner Radress_city;
+    ;
+    Spinner Radress_district;
 
     Button confirm;
     Button cancel;
@@ -54,8 +66,12 @@ public class GenerateQRActivity extends AppCompatActivity {
         Rphone = (EditText) findViewById(R.id.Rphone);
         RDadress = (EditText) findViewById(R.id.RDadress);
 
-        Sadress = (Spinner) findViewById(R.id.Sadress);
-        Radress = (Spinner) findViewById(R.id.Radress);
+        Sadress_province = (Spinner) findViewById(R.id.Sadress_province);
+        Sadress_city = (Spinner) findViewById(R.id.Sadress_city);
+        Sadress_district = (Spinner) findViewById(R.id.Sadress_district);
+        Radress_province = (Spinner) findViewById(R.id.Radress_province);
+        Radress_city = (Spinner) findViewById(R.id.Radress_city);
+        Radress_district = (Spinner) findViewById(R.id.Radress_district);
 
         confirm = (Button) findViewById(R.id.confirm);
         cancel = (Button) findViewById(R.id.cancel);
@@ -68,9 +84,9 @@ public class GenerateQRActivity extends AppCompatActivity {
                     //输入框不能为空
                     if (TextUtils.isEmpty(Sname.getText()) || TextUtils.isEmpty(Sphone.getText()) || TextUtils.isEmpty(SDadress.getText()) ||
                             TextUtils.isEmpty(Rname.getText()) || TextUtils.isEmpty(Rphone.getText()) || TextUtils.isEmpty(RDadress.getText())) {
-                        Toast.makeText(GenerateQRActivity.this,"null",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(GenerateQRActivity.this, "null", Toast.LENGTH_SHORT).show();
                     } else {
-                        Log.e("GenerateQRActivity","name="+Sname.getText().toString()+" phone="+Sphone.getText().toString()+" adress="+SDadress.getText().toString());
+                        Log.e("GenerateQRActivity", "name=" + Sname.getText().toString() + " phone=" + Sphone.getText().toString() + " adress=" + SDadress.getText().toString());
                         createCode(v);
                     }
                 } catch (UnsupportedEncodingException e) {
@@ -85,7 +101,190 @@ public class GenerateQRActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //发件详细地址
+        Sadress_province.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Message shengmessage = new Message();
+                shengmessage.what = (int) id;
+                a = shengmessage.what;
+                shenghandler.sendMessage(shengmessage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        Sadress_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Message shimessage = new Message();
+                shimessage.what = a * 10 + (int) id;
+                shihandler.sendMessage(shimessage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //收件详细地址
+        Radress_province.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Message shengmessage = new Message();
+                shengmessage.what = (int) id;
+                a = shengmessage.what;
+                shenghandler2.sendMessage(shengmessage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        Radress_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Message shimessage = new Message();
+                shimessage.what = a * 10 + (int) id;
+                shihandler2.sendMessage(shimessage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
     }
+
+    //Spinner 寄件人省选择
+    @SuppressLint("HandlerLeak")
+    private Handler shenghandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    SpinnerAdapter shi0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.henanshi, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_city.setAdapter(shi0Adapter);
+                    SpinnerAdapter qu0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.zhengzhouqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu0Adapter);
+                    break;
+                case 1:
+                    SpinnerAdapter shiAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.anhuishi, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_city.setAdapter(shiAdapter);
+                    SpinnerAdapter quAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.hefeiqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(quAdapter);
+                    break;
+                case 2:
+                    SpinnerAdapter shi2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.shanxishi, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_city.setAdapter(shi2Adapter);
+                    SpinnerAdapter qu2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu2Adapter);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+    //Spinner 寄件人市选择
+    @SuppressLint("HandlerLeak")
+    private Handler shihandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    SpinnerAdapter qu0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.zhengzhouqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu0Adapter);
+                    break;
+                case 1:
+                    SpinnerAdapter quAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.kaifengqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(quAdapter);
+                    break;
+                case 10:
+                    SpinnerAdapter qu1Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.hefeiqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu1Adapter);
+                    break;
+                case 11:
+                    SpinnerAdapter qu2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.luaniqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu2Adapter);
+                    break;
+                case 20:
+                    SpinnerAdapter qu3Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu3Adapter);
+                    break;
+                case 21:
+                    SpinnerAdapter qu4Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianyangqu, R.layout.support_simple_spinner_dropdown_item);
+                    Sadress_district.setAdapter(qu4Adapter);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    };
+
+    //Spinner 收件人省选择
+    @SuppressLint("HandlerLeak")
+    private Handler shenghandler2 = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    SpinnerAdapter shi0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.henanshi, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_city.setAdapter(shi0Adapter);
+                    SpinnerAdapter qu0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.zhengzhouqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu0Adapter);
+                    break;
+                case 1:
+                    SpinnerAdapter shiAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.anhuishi, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_city.setAdapter(shiAdapter);
+                    SpinnerAdapter quAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.hefeiqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(quAdapter);
+                    break;
+                case 2:
+                    SpinnerAdapter shi2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.shanxishi, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_city.setAdapter(shi2Adapter);
+                    SpinnerAdapter qu2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu2Adapter);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+    //Spinner 收件人市选择
+    @SuppressLint("HandlerLeak")
+    private Handler shihandler2 = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    SpinnerAdapter qu0Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.zhengzhouqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu0Adapter);
+                    break;
+                case 1:
+                    SpinnerAdapter quAdapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.kaifengqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(quAdapter);
+                    break;
+                case 10:
+                    SpinnerAdapter qu1Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.hefeiqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu1Adapter);
+                    break;
+                case 11:
+                    SpinnerAdapter qu2Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.luaniqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu2Adapter);
+                    break;
+                case 20:
+                    SpinnerAdapter qu3Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu3Adapter);
+                    break;
+                case 21:
+                    SpinnerAdapter qu4Adapter = ArrayAdapter.createFromResource(GenerateQRActivity.this, R.array.xianyangqu, R.layout.support_simple_spinner_dropdown_item);
+                    Radress_district.setAdapter(qu4Adapter);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    };
 
     @Override
     protected void onDestroy() {
@@ -103,11 +302,21 @@ public class GenerateQRActivity extends AppCompatActivity {
         BitMatrix matrix;
         //拼接生成的信息
         StringBuilder stringBuilder = new StringBuilder("fangwolf shi handsome&");
-        stringBuilder.append((Sname.getText())+"&").append((Sphone.getText())+"&").append((Sadress.getSelectedItem()+"&")).append((SDadress.getText()+"&"))
-                .append((Rname.getText())+"&").append((Rphone.getText()+"&")).append((Radress.getSelectedItem()+"&")).append(RDadress.getText());
+        stringBuilder.append((Sname.getText()) + "&")
+                .append((Sphone.getText()) + "&")
+                .append(Sadress_province.getSelectedItem())
+                .append(Sadress_city.getSelectedItem())
+                .append(Sadress_district.getSelectedItem() + "&")
+                .append((SDadress.getText() + "&"))
+                .append((Rname.getText()) + "&")
+                .append((Rphone.getText() + "&"))
+                .append(Radress_province.getSelectedItem())
+                .append(Radress_city.getSelectedItem())
+                .append(Radress_district.getSelectedItem() + "&")
+                .append(RDadress.getText());
         try {
             matrix = new MultiFormatWriter().encode(new String(stringBuilder.toString().getBytes("UTF-8"),
-                            "ISO-8859-1"),BarcodeFormat.QR_CODE, 1000, 1000);
+                    "ISO-8859-1"), BarcodeFormat.QR_CODE, 1000, 1000);
             BarcodeEncoder encoder = new BarcodeEncoder();
             bitmap = encoder.createBitmap(matrix);
             QR.setImageBitmap(bitmap);
